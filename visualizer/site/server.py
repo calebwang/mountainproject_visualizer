95b48ec7a1bfdb1b6f6a6a7f4f3a52b72928a424
Caleb Wang <caleb@caleb.wang>
2019-06-05 21:50:33 -0700

    finally set up build and structure okay

--- visualizer/site/server.py
+++ visualizer/site/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 import mountainproject as mp
 
-app = Flask(__name__, static_folder="../static", template_folder="../templates")
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def index():
---
+++ visualizer/site/webpack.config.js
@@ -0,0 +1,11 @@
+const webpack = require("webpack");
+const resolve = require("path").resolve;
+
+
+module.exports = {
+  entry: __dirname + "/src/visualize.js",
+  output: {
+    path: resolve("./static/build"),
+    filename: "bundle.js"
+  }
+}
