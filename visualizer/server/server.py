836baa931b2734dbb1921bf1c0e3027329086027
Caleb Wang <caleb@caleb.wang>
2019-06-05 20:53:54 -0700

    initial structure for visualizer site

---
+++ visualizer/server/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import mountainproject as mp
+
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
+
+@app.route("/")
+def index():
+  return "Hello World"
+
+@app.route("/pyramid/<user_id>")
+def ticks(user_id):
+  client = mp.Client()
+  user = client.users.get(user_id)
+  return render_template("pyramid.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
