fd1bc3a2ad3bab9e95cc9b449b7c40e60b5c4f26
Caleb Wang <caleb@caleb.wang>
2019-06-07 00:05:12 -0700

    second graph!

--- site/src/visualize.js
+++ site/src/visualize.js
@@ -5,17 +5,30 @@ global.initVisualization = function(data) {
   global.dc = dc;
   document.body.appendChild(document.createTextNode(JSON.stringify(data, undefined, 4)));
 
-  const chart1 = dc.barChart("#chart1");
-  const ndx = dc.crossfilter(data);
+  const ndx = crossfilter(data);
   const dateDimension = ndx.dimension(function(d) { return new Date(d.date); }); 
   const dateGroup = dateDimension.group();
+
+  const typeDimension = ndx.dimension(function(d) { return d.route_type });
+  const typeBuckets = ["RouteType.Sport", "RouteType.Boulder"];
+  const typeGroup = typeDimension.group();
   
+  const chart1 = dc.barChart("#chart1");
   chart1
     .width(1000)
     .height(500)
-    .x(dc.d3.scaleTime().domain([new Date(2018, 0, 1), new Date(2020, 0, 1)]))
+    .x(d3.scaleTime().domain([new Date(2018, 0, 1), new Date(2020, 0, 1)]))
     .dimension(dateDimension)
     .group(dateGroup);
   chart1.render();
-  console.log("HELLO");
+
+  const chart2 = dc.rowChart("#chart2");
+  chart2
+    .width(500)
+    .height(250)
+    .dimension(typeDimension)
+    .group(typeGroup);
+  
+
+  chart2.render();
 }
